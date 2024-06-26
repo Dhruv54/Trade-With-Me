b31e1362193498a07f4dcc80b32f0de11538496e
Dhruv Patel <190310patel@gmail.com>
2024-06-27 00:51:23 +0530

    update fyers package

--- pages/api/getfunds.js
+++ pages/api/getfunds.js
@@ -1,24 +1,31 @@
 const FyersAPI = require("fyers-api-v3").fyersModel;
+const fs = require('fs');
 
+async function readTextFromFileAndParseJSON(filePath) {
+    try {
+        const data = await fs.promises.readFile(filePath, 'utf8');
+        console.log('Data read from file and parsed into JSON:', data);
+        return data;
+    } catch (error) {
+        console.error('Error reading file or parsing data into JSON:', error);
+        return null;
+    }
+}
 // Define the API route handler
 const getfunds = async (req, res) => {
     try {
-        if (req.method === 'POST') {
+        if (req.method === 'GET') {
             // Initialize FyersAPI
             const fyers = new FyersAPI();
             // Set FyersAPI configurations
             fyers.setAppId(process.env.FYERS_APP_ID);
             fyers.setRedirectUrl(process.env.FYERS_REDIRECT_URL);
-           // fyers.setAppId("5SLN8FHRBC-100");
-           // fyers.setRedirectUrl("https://trade.fyers.in/api-login/redirect-uri/index.html");
-           const filePath='access_token.txt';
-           const data = await readTextFromFileAndParseJSON(filePath)
-           console.log(data);
-           const jsonData = JSON.parse(data);
-           console.log(jsonData);
-           fyers.setAccessToken(jsonData.access_token);
 
-           // fyers.setAccessToken("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGkuZnllcnMuaW4iLCJpYXQiOjE3MTA2ODA4NDUsImV4cCI6MTcxMDcyMTgyNSwibmJmIjoxNzEwNjgwODQ1LCJhdWQiOlsieDowIiwieDoxIiwieDoyIiwiZDoxIiwiZDoyIiwieDoxIiwieDowIl0sInN1YiI6ImFjY2Vzc190b2tlbiIsImF0X2hhc2giOiJnQUFBQUFCbDl1c05BdF9oNmY1VnMzWVBadTI2WmZNX3dlV09xeWFKRXVHZGU4S1ItRnRkZmJTSUpLdlgzeUlqNXNwNlJXaV9uSG5zSTBjMEZEa1ZYaDVKaHhwREQ1Z3pTamtfTG5wOXZPazQzTDg0NVJfNF9Zbz0iLCJkaXNwbGF5X25hbWUiOiJESFJVViBKQVlBTlRJQkhBSSBQQVRFTCIsIm9tcyI6IksxIiwiaHNtX2tleSI6bnVsbCwiZnlfaWQiOiJZRDAzNjkwIiwiYXBwVHlwZSI6MTAwLCJwb2FfZmxhZyI6Ik4ifQ.OFI1thwPJDDsjujPOivt7iQl3wA4ZWYwlkiGOhWP7tY");
+            
+            const filePath='access_token.txt';
+            const data = await readTextFromFileAndParseJSON(filePath)
+            const jsonData = JSON.parse(data);
+            fyers.setAccessToken(jsonData.access_token);
 
             // Make the API request to get profile
             const funds = await fyers.get_funds();
---
+++ pages/funds.js
@@ -0,0 +1,52 @@
+import React,{useState, useEffect} from 'react'
+
+const Funds = (user) => {
+
+  const [fundLimit, setfundLimit] = useState()
+
+  const handlegetfunds = async (e) => {
+    try {
+      const response = await fetch('/api/getfunds', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        setfundLimit(data.funds.fund_limit)
+        console.log('fetch fund successful:', data);
+      } else {
+        console.error('fetch profile failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error during fetch profile:', error);
+    }
+  };
+
+  useEffect(() => { 
+    if(user)
+        handlegetfunds(); 
+  }, []); // Empty dependency array ensures this runs only once
+
+  return (
+    <>
+      {fundLimit && (
+        <div className="bg-white p-6 rounded-lg shadow-md">
+        <h2 className="text-xl font-bold mb-4">Fund Limits</h2>
+        <div className="grid grid-cols-2 gap-4">
+          {fundLimit.map((limit) => (
+            <div key={limit.id}>
+              <p className="text-gray-700 font-semibold">{limit.title}:</p>
+              <p className="text-gray-900">{`Equity: ${limit.equityAmount.toFixed(2)}`}</p>
+              <p className="text-gray-900">{`Commodity: ${limit.commodityAmount.toFixed(2)}`}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      )}
+    </>
+  )
+}
+
+export default Funds
